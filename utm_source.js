8d67b635b9443dea797e28ebe3900028a2ad8ce3
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2023-03-24 11:55:13 +0200

    Update utm_source.js

--- utm_source.js
+++ utm_source.js
@@ -1,16 +1,18 @@
+const TAG = 'utm_source'
+
 const params = new URLSearchParams(window.location.search);
 const url = new URL(window.location.href)
-const utm_source = sessionStorage.getItem("utm_source")
-if (!utm_source && params.get('utm_source')) {
-  sessionStorage.setItem('utm_source', params.get('utm_source'))
-  const session = sessionStorage.getItem('utm_source')
+const utm_source = sessionStorage.getItem(TAG)
+if (!utm_source && params.get(TAG)) {
+  sessionStorage.setItem(TAG, params.get(TAG))
+  const session = sessionStorage.getItem(TAG)
   utm_source = params.get(utm_source)
 }
 document.addEventListener("DOMContentLoaded", () => {
   const forms = document.forms
   for (let form of forms) {
     let input = document.createElement('input')
-    input.setAttribute('name', 'utm_source')
+    input.setAttribute('name', TAG)
     input.setAttribute('value', utm_source)
     input.setAttribute('type', "hidden")
     form.appendChild(input)
